f00653fbf82a5c2a022123e7e9b29fa3832149e0
Anton Kheystver <okolobaxa@gmail.com>
2016-02-23 15:49:01 +0300

    Initial commit

---
+++ OpenGraphMeta.magic.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ *
+ */
+
+$magicWords = array();
+
+/** English (English) */
+$magicWords['en'] = array(
+	'setmainimage' => array( 0, 'setmainimage' ),
+);
+
+/** Arabic (العربية) */
+$magicWords['ar'] = array(
+	'setmainimage' => array( 0, 'ضبط_صورة_رئيسية' ),
+);
+
+/** Egyptian Arabic (مصرى) */
+$magicWords['arz'] = array(
+	'setmainimage' => array( 0, 'ضبط_صوره_رئيسيه' ),
+);
+
+/** German (Deutsch) */
+$magicWords['de'] = array(
+	'setmainimage' => array( 0, 'setze_hauptbild' ),
+);
+
+/** Zazaki (Zazaki) */
+$magicWords['diq'] = array(
+	'setmainimage' => array( 0, 'sazéseréresiman' ),
+);
+
+/** Persian (فارسی) */
+$magicWords['fa'] = array(
+	'setmainimage' => array( 0, 'تنظیم_نگاره_اصلی' ),
+);
+
+/** French (français) */
+$magicWords['fr'] = array(
+	'setmainimage' => array( 0, 'imageprincipale' ),
+);
+
+/** Korean (한국어) */
+$magicWords['ko'] = array(
+	'setmainimage' => array( 0, '주그림설정' ),
+);
+
+/** Macedonian (македонски) */
+$magicWords['mk'] = array(
+	'setmainimage' => array( 0, 'поставиглавнастраница' ),
+);
+
+/** Malayalam (മലയാളം) */
+$magicWords['ml'] = array(
+	'setmainimage' => array( 0, 'പ്രധാനചിത്രംസജ്ജീകരിക്കൽ' ),
+);
+
+/** Dutch (Nederlands) */
+$magicWords['nl'] = array(
+	'setmainimage' => array( 0, 'hoofdafbeeldinginstellen' ),
+);
+
+/** Russian (русский) */
+$magicWords['ru'] = array(
+	'setmainimage' => array( 0, 'установитьглавноеизображение', 'установить_главное_изображение' ),
+);
+
+/** Serbian (Cyrillic script) (српски (ћирилица)‎) */
+$magicWords['sr-ec'] = array(
+	'setmainimage' => array( 0, 'поставиглавнуслику', 'постави_главну_слику' ),
+);
+
+/** Serbian (Latin script) (srpski (latinica)‎) */
+$magicWords['sr-el'] = array(
+	'setmainimage' => array( 0, 'postavi_glavnu_sliku' ),
+);
+
+/** Ukrainian (українська) */
+$magicWords['uk'] = array(
+	'setmainimage' => array( 0, 'встановити_головне_зображення' ),
+);
+
+/** Simplified Chinese (中文（简体）‎) */
+$magicWords['zh-hans'] = array(
+	'setmainimage' => array( 0, '设置主图像' ),
+);
+
+/** Traditional Chinese (中文（繁體）‎) */
+$magicWords['zh-hant'] = array(
+	'setmainimage' => array( 0, '設定主要圖像' ),
+);
